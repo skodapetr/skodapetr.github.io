0fe1ed1ce0d774d1fdebda137a56f1a79fab4d33
Petr Škoda <skodapetr@gmail.com>
2022-05-25 22:14:36 +0200

    Add presentation template

---
+++ assets/presentations.js
@@ -0,0 +1,16 @@
+(function main() {
+
+  window.MainPresentation = {
+    "configuration": {
+      // Use all the available space.
+      "width": "100%",
+      "height": "100%",
+      //
+      "hash": true,
+      "slideNumber": "c",
+      "navigationMode": "linear",
+      "plugins": [ RevealHighlight, RevealNotes ],
+    }
+  };
+
+})();
