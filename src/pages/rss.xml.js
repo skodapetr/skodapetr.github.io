f37a78adfbcdb5c625da361fcbb7340966295a8a
Petr Škoda <skodapetr@gmail.com>
2024-11-16 10:17:41 +0100

    Remove use of constants

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,12 +1,11 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
-import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
 
 export async function GET(context) {
 	const posts = await getCollection("post");
 	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
+		title: "Škoda Petr",
+		description: "My personal website.",
 		site: context.site,
 		items: posts.map((post) => ({
 			...post.data,
